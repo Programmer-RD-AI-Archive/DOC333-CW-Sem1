7c98455d296da0789c48cddb6ea2e021e3e90fed
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2023-11-25 16:04:25 +0530

    Update help_functions.py

--- Corporation/help_functions.py
+++ Corporation/help_functions.py
@@ -4,7 +4,7 @@ from Corporation import *
 def menu(
     redirect: bool = False,
     to: int = None,
-    company_name: str = company_name,
+    name_of_the_company: str = company_name,
     msg: str = "Enter your choice: ",
 ) -> str:
     """This function finds the choice that should be displayed to the user next...
@@ -12,13 +12,13 @@ def menu(
     Keyword arguments:
     redirect (bool) -- A boolean to know whether or not the user will be redirected
     to (int) -- The choice that user will be redirected to
-    company_name (str) -- The companies name that will be displayed
+    name_of_the_company (str) -- The companies name that will be displayed
     msg (str) -- The message that will be displayed to the user asking their next choice
 
     Return: (str) the next choice which the user has chosen...
     """
     main_menu = f"""
-     {company_name}
+     {name_of_the_company}
      Main Menu
      1. Add a new project to existing projects.
      2. Remove a completed project from existing projects.
